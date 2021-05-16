76d8c6bebd8d0d69e6a113fb6072d76788055c54
judge2010 <74088083+judge2010@users.noreply.github.com>
2021-05-16 13:42:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function fruit(){
+window.location="fruit.html";
+};
+function desk(){
+    window.location="Desk.html";
+    };
+    function bottles(){
+        window.location="Bottles.html";
+        };
+        function tA(){
+            window.location="t a.html";
+            };
+            function bed(){
+                window.location="Bed.html";
+                };
